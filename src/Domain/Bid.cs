c74a75be5e38085aa5e2360f12e0a5c68e6a728a
Karolina Simcic <kgsimcic@gmail.com>
2024-06-28 09:09:56 -0400

    modified Bid and Rule to have consistent naming across entity, controller, etc

---
+++ src/Domain/Bid.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Numerics;
+
+namespace Dot.Net.WebApi.Domain
+{
+    public class Bid
+    {
+        public int BidListId { get; set; }
+        public string Account {  get; set; }
+        public string Type { get; set; }
+        public decimal? BidQuantity { get; set; }
+        public decimal? AskQuantity { get; set; }
+        public decimal? Bid { get; set; }
+        public decimal? Ask { get; set; }
+        public string? Benchmark { get; set; }
+        public DateTime BidListDate { get; set; }
+        public string? Commentary { get; set; }
+        public string? Security { get; set; }
+        public string? Status { get; set; }
+        public string? Trader { get; set; }
+        public string? Book { get; set; }
+        public string? CreationName { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string? RevisionName { get; set; }
+        public DateTime? RevisionDate { get; set; }
+        public string? DealName { get; set; }
+        public string? DealType { get; set; }
+        public string? SourceListId { get; set; }
+        public string? Side { get; set; }
+
+    }
+}
